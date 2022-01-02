9d6fd9fecd6123b9256b557c9fbdf44d6620c759
Giovani <giovanicnvg@gmail.com>
2022-01-02 14:50:08 -0600

    Added composer-autoload. First scss compilation with gulp. includes/database setted.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6b17d4cfe80213abf7232c6a2af1579894e0c7fb',
+        'name' => 'juandelatorre/up-task_mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6b17d4cfe80213abf7232c6a2af1579894e0c7fb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
