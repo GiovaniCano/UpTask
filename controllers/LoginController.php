bd0dc9d7d7d3c12d7ff97f5cdbc1c6fc7f4b9200
Giovani <giovanicnvg@gmail.com>
2022-01-11 23:26:59 -0600

    Metodos de LoginController, pagina de login y htaccess

---
+++ controllers/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+    public static function login(Router $router) {
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            
+        }
+
+        // Render a la vista
+        $router->render("auth/login", [
+            "titulo" => "Iniciar Sesión"
+        ]);
+    }
+
+    public static function logout() {
+        echo "desde logout";
+    }
+
+    public static function crear(Router $router) {
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            
+        }
+
+        // Render a la vista
+        $router->render("auth/crear", [
+            "titulo" => "Crea tu cuenta"
+        ]);
+    }
+
+    public static function olvide() {
+        echo "desde olvide";
+
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            
+        }
+    }
+
+    public static function reestablecer() {
+        echo "desde reestablecer";
+
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            
+        }
+    }
+        
+    public static function mensaje() {
+        echo "desde mensaje";
+    }
+        
+    public static function confirmar() {
+        echo "desde confirmar";
+    }
+}
