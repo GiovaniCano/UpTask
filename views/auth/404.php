9f7d0363e336b6c01642891d69eb12a58eb526be
Giovani <giovanicnvg@gmail.com>
2022-02-03 23:20:16 -0600

    fixes to page404

--- views/auth/404.php
+++ views/auth/404.php
@@ -1,11 +1,11 @@
-<div class="contenedor confirmar">
+<div class="contenedor p404">
     <?php include_once __DIR__ . "/../templates/nombre-sitio.php"?>
 
     <div class="contenedor-sm">      
-        <h3 style="text-align: center; color: white;">404<span style="font-weight: 400; font-size: 22px;"> | Página no Encontrada</span></h3>
+        <h3>404<span> | Página no Encontrada</span></h3>
     
         <div class="acciones">
-            <a href="/">Iniciar Sesión</a>
+            <a href="/">Volver</a>
         </div>
     </div> <!--contenedor-sm-->
 </div>
